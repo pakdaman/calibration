6797483d0ca5ce6dea48764e7d427aad1dd4ecc2
Mahdi Pakdaman <pakdaman@gmail.com>
2016-12-19 18:41:56 -0500

    enir R Package

---
+++ ENIR/Matlab/test.m
@@ -0,0 +1,52 @@
+clear; clc;
+load data;
+
+% %% Apply linear SVM regression model to make predictions
+% disp('Using Linear SVM ')
+% svmStruct = svmtrain(XTR,YTR,'ShowPlot',true);
+% title('Linear SVM')
+% PTR = Mysvmclassify(svmStruct,XTR);
+% PTR = exp(PTR)./(1+exp(PTR));% Convert output of SVM to uncalibrated probs 
+% PTE = Mysvmclassify(svmStruct,XTE);
+% PTE = exp(PTE)./(1+exp(PTE));% Convert output of SVM to uncalibrated probs 
+% 
+% disp('Performance of (Linear) SVM probabilities : ')
+% M = getMeasures(PTE,YTE)
+% % Build ENIR Model
+% ENIR = build(PTR, YTR);
+% PTE_enir = predict(ENIR, PTE, 1);
+% disp('Performance of Calibrated Probabilities using ENIR : ')
+% M_enir = getMeasures(PTE_enir,YTE)
+
+
+%% Apply Quadratic SVM regression model to make predictions
+disp('Using Quadratic SVM ')
+figure;
+svmStruct = svmtrain(XTR,YTR,'Kernel_Function','polynomial','Polyorder',2, 'ShowPlot',true);
+title('Quadratic SVM')
+
+PTR = Mysvmclassify(svmStruct,XTR);
+PTR = exp(PTR)./(1+exp(PTR));% Convert output of SVM to uncalibrated probs 
+PTE = Mysvmclassify(svmStruct,XTE);
+PTE = exp(PTE)./(1+exp(PTE));% Convert output of SVM to uncalibrated probs 
+
+disp('Performance of (Quadratic) SVM probabilities : ')
+M = getMeasures(PTE,YTE)
+% Build BBQ Model
+ENIR = build(PTR, YTR);
+
+% Calibrated the prediction using BBQ
+PTE_enir = predict(ENIR, PTE, 1);
+disp('Performance of Calibrated Probabilities using ENIR : ')
+M_enir = getMeasures(PTE_enir,YTE)
+
+disp('End!')
+
+
+dlmwrite('test_data_y.csv', PTE,'precision', 50);
+dlmwrite('test_data_z.csv', YTE,'precision', 50);
+
+dlmwrite('train_data_y.csv', PTR,'precision', 50);
+dlmwrite('train_data_z.csv', YTR,'precision', 50);
+
+
---
+++ ENIR/R/README.md
@@ -0,0 +1,44 @@
+Preface 
+===========
+This is a research code and there is not guarantee on using the code.
+This code includes an R package "enir" that implements our ENIR calibration method in our ICDM 2016 paper.
+To run the code you should first install the neariso R package referenced inside our icdm paper. 
+For the sake of completeness, a version of neariso R package that is used in our experiments is 
+also provided in this repository (neariso_1.0.tar.gz). You should then install the enir package which is also included in this
+repository (enir_1.0.tar.gz).
+
+
+
+Two main methods:
+===========
++ enir::build(yTrain, zTrain);
+Input:
+
+Output:
+
+
+
+
++ enir::predict(enirModel, yTest);
+Input: 
+enirModel: a ENIR calibration model generated using the "enir::build()" method
+yTest: vector of uncalibratd classification scores
+
+output:
+res: corresponding calibrated scores obtained using the ENIR calibration model
+
+
++ enir:getMeasures(y, z);
+Input:
+y: vector of predictions (classification scores) which is in the interval [0, 1]
+z: vector of true class of instances {0,1}
+
+Output: 
+a list of evaluation measures including (RMSE, AUC, ACC, MCE, ECE)
+
+References 
+===========
+
+Mahdi Pakdaman Naeini, Gregory F. Cooper. "Binary Classifier Calibration using an Ensemble of Near Isotonic Regression Models”, 
+IEEE Internation Conference on Data Mining (ICDM), Barcelona, Spain, December 2016.
+
